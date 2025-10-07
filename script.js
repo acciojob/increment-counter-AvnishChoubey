49f33aadab8a1952ddeac5cfbaa7a88465691dad
Avnish Choubey <118668098+AvnishChoubey@users.noreply.github.com>
2025-10-08 02:28:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const counterElement = document.getElementById('counter');
+const incrementButton = document.getElementById('increment');
+
+let count = 0;
+
+incrementButton.addEventListener('click', () => {
+  count++;
+	counterElement.textContent = count;
+});
+counterElement.textContent = count;
